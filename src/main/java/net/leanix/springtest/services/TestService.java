ba29196c8d2320fec40fe6ec787c29967f9ded58
Junhong Liang <junhong.liang@leanix.net>
2021-03-10 13:18:40 +0100

    Add hystrix dependency

--- src/main/java/net/leanix/springtest/services/TestService.java
+++ src/main/java/net/leanix/springtest/services/TestService.java
@@ -1,5 +1,6 @@
 package net.leanix.springtest.services;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import net.leanix.springtest.dao.PersonDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,12 +13,17 @@ public class TestService {
     private PersonDao personDao;
 
     @Transactional
+    @HystrixCommand(fallbackMethod = "getServerNameFallback")
     public String getServerName() {
         personDao.add("AA2", "BB2");
         if (true) {
             throw new RuntimeException();
         }
         return "Spring Test";
+    }
+
+    public String getServerNameFallback() {
+        return "Fall Back";
     }
 
 }
